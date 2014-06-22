5bd7a7c3644b9edacfbf0e01f8ffcbef7def125c
Florian Goessler <mail@floriangoessler.de>
2014-06-22 19:36:51 +0200

    config for remote server

--- groupplaner/www/js/support/config.js
+++ groupplaner/www/js/support/config.js
@@ -3,8 +3,11 @@ window.app = window.app || {};
 app.groupplaner = app.groupplaner || {};
 app.groupplaner.config = app.groupplaner.config || {};
 
+app.groupplaner.config.sourcekey = "GroupplanerApp";
+
 // url config
-app.groupplaner.config.baseUrl = 'http://localhost:8080/user';
+// app.groupplaner.config.baseUrl = 'http://localhost:8080/user';  	// local
+app.groupplaner.config.baseUrl = 'http://zibet.soft.cs.uni-potsdam.de:8080/GroupplanerServer/user';		//remote
 
 
 
